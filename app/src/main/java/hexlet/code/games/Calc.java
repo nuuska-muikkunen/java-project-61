a1bb51503b2ed36813ef4bc4bea34c176d9d1044
Boris Sturua <stboris65@gmail.com>
2023-07-06 09:59:20 +0300

    chaged defaul branch in case operator

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -16,7 +16,7 @@ public class Calc {
             case "*":
                 return firstArgument * secondArgument;
             default:
-                return firstArgument / 0;
+                return Integer.MAX_VALUE/*firstArgument / 0*/;
         }
     }
     public static String[] generateQuestion() {
